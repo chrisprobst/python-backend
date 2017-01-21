671f64a4bf4caad7d86dc48be137f7602015e598
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-01-21 01:39:14 +0100

    Add v1 of HHC user data tables

--- backend/database/Database.py
+++ backend/database/Database.py
@@ -11,7 +11,67 @@ class Database(object):
 
 	def createTables(self):
 		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `contact` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`prefix` TEXT,
+				`first_name` TEXT,
+				`last_name` TEXT,
+				`birth_date` DATE,
+				`comment` TEXT
+		)""")
 
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `mail` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`contact_id` INTEGER REFERENCES contact(id),
+				`description` TEXT,
+				`address` TEXT
+		)""")
+
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `phone` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`contact_id` INTEGER REFERENCES contact(id),
+				`description` TEXT,
+				`number` TEXT
+		)""")
+
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `address` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`contact_id` INTEGER REFERENCES contact(id),
+				`description` TEXT,
+				`street` TEXT,
+				`number` INTEGER(8),
+				`addr_extra` TEXT,
+				`postal` TEXT,
+				`city` TEXT
+		)""")
+
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `study` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`contact_id` INTEGER REFERENCES contact(id),
+				`status` TEXT,
+				`school` TEXT,
+				`course` TEXT,
+				`start` DATE,
+				`end` DATE,
+				`focus` TEXT,
+				`degree` TEXT
+		)""")
+
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `member` (
+				`contact_id` INTEGER PRIMARY KEY REFERENCES contact(id),
+				`ressort` TEXT,
+				`active` INTEGER(1),
+				`position` TEXT,
+				`joined` DATE,
+				`left` DATE
+		)""")
+
+		self.cursor.execute(
 			"""CREATE TABLE IF NOT EXISTS `users` (
 			`username` TEXT PRIMARY KEY,
 			`passhash` TEXT,
@@ -19,8 +79,8 @@ class Database(object):
 			`created` DATE DEFAULT CURRENT_TIMESTAMP
 
 		)""")
-		self.cursor.execute(
 
+		self.cursor.execute(
 			"""CREATE TABLE IF NOT EXISTS `api_tokens` (
 			`username` TEXT,
 			`api_token` TEXT,
