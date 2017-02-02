f9899561b7a20589577395ebd76dae362f5ed976
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-02-02 14:16:45 +0100

    Make Passhash.verify static

--- backend/webhandler/util/PasshashVerifier.py
+++ backend/webhandler/util/PasshashVerifier.py
@@ -1,9 +1,8 @@
-#! /usr/bin/python
-# -*- coding: iso-8859-1 -*-
-
 from passlib.hash import pbkdf2_sha256
 
+
 class PasshashVerifier(object):
-	
-	def verify(self, password, salt, passhash):
-		return pbkdf2_sha256.verify(salt+password, passhash)
+
+    @staticmethod
+    def verify(password, salt, passhash):
+        return pbkdf2_sha256.verify(salt + password, passhash)
