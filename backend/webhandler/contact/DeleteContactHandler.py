170559da947c4cfa9907d287a0d76e4917fe2298
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-02-09 11:16:14 +0100

    DeleteContactHandler

--- backend/webhandler/contact/DeleteContactHandler.py
+++ backend/webhandler/contact/DeleteContactHandler.py
@@ -1,6 +1,8 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
 import json
 
-import tornado.web
 import tornado.escape
 
 from backend.webhandler.util import ApiHandler
@@ -10,6 +12,10 @@ from backend.database.controller import ContactController
 class DeleteContactHandler(ApiHandler.ApiHandler):
 
     def post(self):
+        """
+        Post handler for DeletecontactHandler
+        :return: (none)
+        """
         if self.api_token_is_invalid():
             self.write_invalid_api_token_response()
             return
@@ -22,6 +28,10 @@ class DeleteContactHandler(ApiHandler.ApiHandler):
             self.write_error_response(e)
     
     def write_success_response(self):
+        """
+        Write a JSON answer containing no error messages
+        :return: (none)
+        """
         data = {
             "error": None
         }
