69b7475ef505b20a4b246f985f3c9c5599e2fef0
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-02-10 20:04:58 +0100

    WebhandlerContext

--- backend/config/WebhandlerContext.py
+++ backend/config/WebhandlerContext.py
@@ -2,6 +2,7 @@
 # -*- coding: iso-8859-1 -*-
 
 class WebhandlerContext(object):
+    
     def __init__(self, config, database, logger):
         self.config = config
         self.database = database
