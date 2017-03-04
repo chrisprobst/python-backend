9748fb4f463fb8203743adbb091f328a35994302
Daniel Högel <hoegel.daniel@gmail.com>
2017-03-04 03:55:43 +0100

    Move handler functions into ApiHandler

--- backend/webhandler/util/ApiHandler.py
+++ backend/webhandler/util/ApiHandler.py
@@ -2,11 +2,46 @@
 # -*- coding: iso-8859-1 -*-
 
 import json
+import tornado.escape
+import traceback
 
 import BaseHandler
 
 
 class ApiHandler(BaseHandler.BaseHandler):
+    
+    def api_post(self, callback, data_key=''):
+        """
+        general api post handler for all handler
+        :return: (none)
+        """
+        if self.api_token_is_invalid():
+            self.write_invalid_api_token_response()
+            return
+        data = tornado.escape.json_decode(self.get_argument("data"))
+        try:
+            if data_key:
+                data = data[data_key]
+            if data:
+                result = callback(data)
+            else:
+                result = callback()
+            self.write_success_response(result)
+        except BaseException, e:
+            print traceback.format_exc()
+            self.write_error_response(e)
+    
+    def write_success_response(self, result):
+        """
+        Write a success JSON response
+        :param result
+        :return: (none)
+        """
+        data = {
+            "result": result,
+            "error": None
+        }
+        self.write(json.dumps(data))
     
     LOG_INVALID_API_TOKEN = "Received invalid api token {token}"
 
