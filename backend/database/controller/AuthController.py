042ea16253e71d3f0b94abad82ede02953a365c9
Robin <robinthues@gmail.com>
2017-03-05 21:17:49 +0100

    AuthController created
    created AuthController, seperated DB access from handler

---
+++ backend/database/controller/AuthController.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+import json
+import os
+
+from backend.webhandler.util import BaseHandler
+from backend.webhandler.util import PasshashVerifier
+
+class AuthController(object):
+
+    @staticmethod
+    def generate_api_token():
+        """
+        Generates a 128 bit nonce as a string that only contains hex digits based on os.random
+        :return: (str) The generated nonce
+        """
+        return os.urandom(128).encode('hex')
+
+    def check_token(self, username, password):
+        if self.is_invalid_username(username):
+            raise Exception("InvalidUser")
+        elif self.is_invalid_password(username, password):
+            raise Exception("InvalidPassword")
+        else:
+            api_token = self.generate_api_token()
+            try:
+                self.store_api_token(username, api_token)
+                self.database.commit()
+                return api_token
+            except BaseException, e:
+                self.database.rollback()
+                raise e
+
+
+    def __init__(self, database):
+        self.database = database
+
+    def is_invalid_username(self, username):
+        """
+        Returns True if given string is not a valid username
+        :param username: (str) The username to test
+        :return: (bool)
+        """
+        query = "SELECT * FROM `users` WHERE username=?;"
+        userdata = self.database.get_single_value_by_query(query, (username,))
+        if userdata is None:
+            return True
+        return False
+
+    def is_invalid_password(self, username, password):
+        """
+        Returns True if the given pair of username and password is not valid
+        :param username: (str) The username to verify
+        :param password: (str) The password to verify
+        :return: (bool)
+        """
+        salt_query = "SELECT salt FROM `users` WHERE username=?"
+        passhash_query = "SELECT passhash FROM `users` WHERE username=?"
+        salt = self.database.get_single_value_by_query(salt_query, (username,))
+        passhash = self.database.get_single_value_by_query(passhash_query, (username,))
+        return not PasshashVerifier.PasshashVerifier.verify(password, salt, passhash)
+
+    def store_api_token(self, username, api_token):
+        """
+        Stores a generated API token in the database and relates it to one database user
+        :param username: (str) The username
+        :param api_token: (str) The API token
+        :return: (none)
+        """
+        query = "INSERT INTO api_tokens (username, api_token) VALUES (?,?);"
+        args = (username, api_token)
+        self.database.cursor.execute(query, args)
+        self.database.commit()
--- backend/webhandler/auth/GenerateApiTokenHandler.py
+++ backend/webhandler/auth/GenerateApiTokenHandler.py
@@ -2,21 +2,16 @@
 # -*- coding: iso-8859-1 -*-
 
 import json
-import os
 
 from backend.webhandler.util import BaseHandler
-from backend.webhandler.util import PasshashVerifier
+
+import tornado.escape
+
+from backend.webhandler.util import ApiHandler
+from backend.database.controller import AuthController
 
 
 class GenerateApiTokenHandler(BaseHandler.BaseHandler):
-    
-    @staticmethod
-    def generate_api_token():
-        """
-        Generates a 128 bit nonce as a string that only contains hex digits based on os.random
-        :return: (str) The generated nonce
-        """
-        return os.urandom(128).encode('hex')
     
     def post(self):
         """
@@ -25,40 +20,25 @@ class GenerateApiTokenHandler(BaseHandler.BaseHandler):
         """
         username = self.get_argument("username")
         password = self.get_argument("password")
-        if self.is_invalid_username(username):
-            self.write_invalid_username_response()
-        elif self.is_invalid_password(username, password):
-            self.write_invalid_password_response()
-        else:
-            api_token = self.generate_api_token()
-            self.store_api_token(username, api_token)
+        ctr = AuthController.AuthController(self.context.database)
+        try:
+            api_token = ctr.check_token(username, password)
             self.write_success_response(api_token)
-    
-    def is_invalid_username(self, username):
-        """
-        Returns True if given string is not a valid username
-        :param username: (str) The username to test
-        :return: (bool)
-        """
-        query = "SELECT * FROM `users` WHERE username=?;"
-        userdata = self.context.database.get_single_value_by_query(query, (username,))
-        if userdata is None:
-            return True
-        return False
-    
-    def is_invalid_password(self, username, password):
-        """
-        Returns True if the given pair of username and password is not valid
-        :param username: (str) The username to verify
-        :param password: (str) The password to verify
-        :return: (bool)
-        """
-        salt_query = "SELECT salt FROM `users` WHERE username=?"
-        passhash_query = "SELECT passhash FROM `users` WHERE username=?"
-        salt = self.context.database.get_single_value_by_query(salt_query, (username,))
-        passhash = self.context.database.get_single_value_by_query(passhash_query, (username,))
-        return not PasshashVerifier.PasshashVerifier.verify(password, salt, passhash)
-    
+        except BaseException, e:
+            exception_text = str(e)
+            if(exception_text == "InvalidUser"):
+                self.write_invalid_username_response()
+            elif(exception_text == "InvalidPassword"):
+                self.write_invalid_password_response()
+            else:
+                self.write_error_response(e)
+
+    def write_error_response(self, e):
+        data = {
+            "error": repr(e)
+        }
+        self.write(json.dumps(data))
+
     def write_invalid_username_response(self):
         """
         Writes a JSON answer with an error message (invalid username)
@@ -90,15 +70,3 @@ class GenerateApiTokenHandler(BaseHandler.BaseHandler):
             "error": None
         }
         self.write(json.dumps(data))
-    
-    def store_api_token(self, username, api_token):
-        """
-        Stores a generated API token in the database and relates it to one database user
-        :param username: (str) The username
-        :param api_token: (str) The API token
-        :return: (none)
-        """
-        query = "INSERT INTO api_tokens (username, api_token) VALUES (?,?);"
-        args = (username, api_token)
-        self.context.database.cursor.execute(query, args)
-        self.context.database.commit()
