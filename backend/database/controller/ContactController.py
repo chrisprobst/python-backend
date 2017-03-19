dd6b9e2e23790b34f52f3f3a52b6beb7a0dd815d
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-03-19 09:13:03 +0100

    Reset ContactController to original version

--- backend/database/controller/ContactController.py
+++ backend/database/controller/ContactController.py
@@ -1,13 +1,8 @@
 #! /usr/bin/python
 # -*- coding: iso-8859-1 -*-
 
-import itertools
-
-from backend.database.controller.BaseController import BaseController
-
 
 class ContactController(object):
-    
     # TODO: Remove 'json' parameters since they are implicit and unuseful
     
     # TODO: Export to BaseController
@@ -19,17 +14,6 @@ class ContactController(object):
         "study": ["contact_id", "status", "school", "course", "start", "end", "focus", "degree"]
     }
     
-    CONTACT_PROFILE = {
-        'primary': {'table': 'contact', 'column': 'id'},
-        'rows': {
-            'mail': {'key': 'contact_id', 'multiple': True},
-            'phone': {'key': 'contact_id', 'multiple': True},
-            'address': {'key': 'contact_id', 'multiple': True},
-            'study': {'key': 'contact_id', 'multiple': True},
-            'member': {'key': 'contact_id', 'multiple': False}
-        }
-    }
-
     # TODO: Export to BaseController
     SELECT_QUERY = "SELECT {columns} FROM `{table}` WHERE {filter};"
     SELECT_QUERY_NO_WHERE = "SELECT {columns} FROM `{table}`;"
@@ -51,13 +35,15 @@ class ContactController(object):
             {"description": "Test Mail1", "address": "alex1@alex.de"}
         ],
         "address": [
-            {"description": "Privat", "street": "Gilbachstrasse", "number": "7-9", "addr_extra": "", "postal": "40219", "city": "Duesseldorf"}
+            {"description": "Privat", "street": "Gilbachstrasse", "number": "7-9", "addr_extra": "", "postal": "40219",
+             "city": "Duesseldorf"}
         ],
         "phone": [
             {"description": "Privat1", "number": "0123456789"}
         ],
         "study": [
-            {"status": "done", "school": "HHU", "course": "Informatik", "start": "dd.mm.yyyy", "end": "dd.mm.yyyy", "focus": "Netzwerksicherheit", "degree": "b_a"}
+            {"status": "done", "school": "HHU", "course": "Informatik", "start": "dd.mm.yyyy", "end": "dd.mm.yyyy",
+             "focus": "Netzwerksicherheit", "degree": "b_a"}
         ]
     }
     
@@ -72,41 +58,68 @@ class ContactController(object):
     # TODO: Database will be passed to BaseController
     def __init__(self, database):
         self.database = database
-        self.base = BaseController
     
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     #                        CREATE METHODS
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
+    
     # TODO: Check if commit field is in every method
     def create_contact(self, contact, commit=True):
         """
         Creates a new contact in the database given a contact structure. Note: since this contact is
         new, no "id" field in the "contact" data is required, just as no "contact_id" fields are
         required in "mail", "phone", "address" oder "study".
-        
+
         :param contact: (dict) A contact structure (no IDs required!)
         :param commit: (bool) If true, all changes will be committed at the end of the function call
         :return: (int) The generated contact id for the new contact record
         """
-        return self.base.create_profile(ContactController.CONTACT_PROFILE, contact, commit)
-
+        try:
+            contact_id = self._insert_json_in_table(contact["contact"], "contact")
+            for mail in contact["mail"]:
+                mail["contact_id"] = contact_id
+                self._insert_json_in_table(mail, "mail")
+            for address in contact["address"]:
+                address["contact_id"] = contact_id
+                self._insert_json_in_table(address, "address")
+            for phone in contact["phone"]:
+                phone["contact_id"] = contact_id
+                self._insert_json_in_table(phone, "phone")
+            for study in contact["study"]:
+                study["contact_id"] = contact_id
+                self._insert_json_in_table(study, "study")
+            if commit:
+                self.database.commit()
+            return contact_id
+        except BaseException, e:
+            self.database.rollback()
+            raise e
+    
     def create_contacts(self, contacts):
         """
         Expects a list of contact structures and calls create_contact() on each of them.
-        
+
         :param contacts: ([dict]) A list of contact structures
         :return: ([int]) A list of the respective contact ids
         """
-        return self.base.create_profiles(ContactController.CONTACT_PROFILE, contacts)
+        contact_ids = []
+        try:
+            for contact in contacts:
+                new_contact_id = self.create_contact(contact, commit=False)
+                contact_ids.append(new_contact_id)
+            self.database.commit()
+            return contact_ids
+        except BaseException, e:
+            self.database.rollback()
+            raise e
     
     # TODO: Shouldn't stay for ever
     def create_dummy_contact(self):
         """
         Creates a dummy contact (a contact with fixed data).
-        
+
         :return: (int) The generated contact id for the new contact record
         """
         self.create_contact(ContactController.DUMMY_CONTACT_DATA)
@@ -124,39 +137,61 @@ class ContactController(object):
         :param contact_id: (int) The contact id to fetch from the database
         :return: (dict) The matching contact structure or an empty dictionary
         """
-        return self.base.select_profile_for_id(contact_id)
+        id_filter = "id={id}".format(id=contact_id)
+        contact_id_filter = "contact_id={id}".format(id=contact_id)
+        result_contact = self._select_named_data(["*"], "contact", id_filter)
+        result_mail = self._select_named_data(["*"], "mail", contact_id_filter)
+        result_phone = self._select_named_data(["*"], "phone", contact_id_filter)
+        result_address = self._select_named_data(["*"], "address", contact_id_filter)
+        result_study = self._select_named_data(["*"], "study", contact_id_filter)
+        if len(result_contact) != 1:
+            return {}
+        contact = {
+            "contact": result_contact[0],
+            "mail": result_mail,
+            "phone": result_phone,
+            "address": result_address,
+            "study": result_study
+        }
+        return contact
     
-    def select_contacts_for_ids(self, contact_ids):
+    def select_contacts_for_ids(self, ids):
         """
         Select multiple contact structures for a given list of IDs.
-        
+
         :param ids: ([int]) List of contact ids to fetch from the database
         :return: ([dict]) A list of matching contact structures (can contain empty dicts for not matching IDs)
         """
-        return self.base.select_profiles_for_ids(contact_ids)
+        contacts = []
+        for contact_id in ids:
+            contacts.append(self.select_contact_for_id(contact_id))
+        return contacts
     
     def select_all_contact_ids(self):
         """
         Selects all existing contact IDs from the database.
-        
+
         :return: ([int]) A list of all existing contact IDs
         """
-        return self.base.select_all_profile_ids(ContactController.CONTACT_PROFILE)
+        self.database.cursor.execute(ContactController.QUERY_SELECT_ALL_CONTACT_IDS)
+        contact_ids = [result[0] for result in self.database.cursor.fetchall()]
+        return contact_ids
     
     def select_all_contacts(self):
         """
         Calls select_contacts_for_ids with select_all_contact_ids() as argument.
-        
+
         :return: ([dict])  A list of all contact structures
         """
-        return self.base.select_all_profiles(ContactController.CONTACT_PROFILE)
+        contact_ids = self.select_all_contact_ids()
+        return self.select_contacts_for_ids(contact_ids)
     
     # TODO: NO HANDLER YET!
     def select_contact_ids_by_filter(self, contact_filter):
         """
         Finds all contact ids of contacts matching the given filter.
         NOTE: The filter structure is not completely done, so please do not use this method by now!
-        
+
         :param contact_filter:
         :return: ([int]) A list of matching contact ids
         """
@@ -257,7 +292,7 @@ class ContactController(object):
         """
         Deletes a given contact structure. NOTE: This method expects a contact structure, not just a
         contact id!
-        
+
         :param contact: (dict) The contact structure to delete
         :return: (None)
         """
@@ -278,7 +313,7 @@ class ContactController(object):
     def _select_named_data(self, columns, table, where=""):
         """
         Select data from a given table and return a labeled data set
-        
+
         :param columns: ([str]) A list of columns to select (NOTE: to select all columns, pass ["*"])
         :param table: (str) Table to select from
         :param where: (str) A valid mysqlite3 WHERE clause. NOTE: if no filter is necessary, you can omit the argument
@@ -306,7 +341,7 @@ class ContactController(object):
     def _insert_json_in_table(self, row, table, commit=False):
         """
         Inserts a dict containing column/value pairs into a given table
-        
+
         :param row: (dict) The data to insert into 'table'
         :param table: (str) The table name to insert the data in
         :param commit: (bool) If True, this method will automatically commit all changes
@@ -319,7 +354,7 @@ class ContactController(object):
         query = ContactController.INSERT_QUERY.format(
             table=table,
             columns=",".join(columns),
-            placeholders=",".join(list("?"*len(columns)))
+            placeholders=",".join(list("?" * len(columns)))
         )
         self.database.cursor.execute(query, values)
         if commit:
@@ -336,7 +371,7 @@ class ContactController(object):
         :param commit: (bool) Commit changes within function call
         :return: (none)
         """
-        columns = filter(lambda col: col in ContactController.TABLE_COLUMNS[table] ,data.keys())
+        columns = filter(lambda col: col in ContactController.TABLE_COLUMNS[table], data.keys())
         updates = ",".join(["{column}=?".format(column=column) for column in columns])
         query = ContactController.UPDATE_QUERY.format(
             table=table,
